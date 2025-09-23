8665f8cbc5031309a7a4e508bdbb03b17cd8d0f8
Md Abu Bokar <mdabu0212@gmail.com>
2025-09-23 23:26:07 +0600

    bg animation and hero section

--- client/src/components/sections/Navbar.tsx
+++ client/src/components/sections/Navbar.tsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between sticky top-0 w-7xl m-auto">
+    <div className="absolute w-full">
+      <div className="flex items-center justify-between w-7xl m-auto sticky top-0 mt-0">
       <div className="text-xl flex items-center gap-0.5 font-medium h-16">
         <Logo></Logo>
         FILLIFY
@@ -14,6 +15,7 @@ export default function Navbar() {
         <Link href="">Edit PDF</Link>
         <Link href="">Make Fillable</Link>
         <Link href="">Contact</Link>
+      </div>
       </div>
     </div>
   );
