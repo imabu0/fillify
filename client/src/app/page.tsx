625904f8442306f98d3616cea9e1ae9fa1e99a60
Md Abu Bokar <mdabu0212@gmail.com>
2025-09-25 22:45:31 +0600

    license and fixes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/sections/Navbar";
 import Hero from "@/components/sections/Hero";
+import About from "@/components/sections/About";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ client/src/components/sections/About.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div>
+      <div>
+        
+      </div>
+      <div></div>
+    </div>
+  );
+}
